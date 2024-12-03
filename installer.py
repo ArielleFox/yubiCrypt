60f038e7bd00e608b8f0cbf4ab5fa9fda8bbe627
Arielle <contact@arielle.quest>
2024-12-03 02:10:06 +0000

    ...

--- installer.py
+++ installer.py
@@ -2,3 +2,11 @@
 import os
 
 os.system('make all')
+os.system('sudo python3 setup_files/install_age.py')
+os.system('cargo install age-plugin-yubikey')
+os.system('sudo python3 setup_files/install_age_plugin.py')
+os.system('python3 setup_files/directory_checker_yubicrypt.py')
+os.system('python3 setup_files/copy_run_files.py')
+os.system('python3 setup_files/directory_checker_dcde.py')
+os.system('python3 setup_files/add_aliases.py')
+
