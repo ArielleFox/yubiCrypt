d7c702e38fc9a61105c7a793e7a33d4079b534f3
Arielle Fox <118761163+ArielleFox@users.noreply.github.com>
2024-12-09 14:47:23 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 import os
 
+os.system('sudo ./install_pcsclite.py')
 os.system('make all')
 os.system('sudo python3 setup_files/install_age.py')
 os.system('cargo install age-plugin-yubikey')
@@ -9,4 +10,3 @@ os.system('python3 setup_files/directory_checker_yubicrypt.py')
 os.system('python3 setup_files/copy_run_files.py')
 os.system('python3 setup_files/directory_checker_dcde.py')
 os.system('python3 setup_files/add_aliases.py')
-
